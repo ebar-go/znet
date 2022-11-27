84ad4aa5c8f4f80a6058270fda4b97d723ae0438
hongker <xiaok2013@live.com>
2022-11-27 20:58:23 +0800

    feat: remove once

--- engine.go
+++ engine.go
@@ -3,32 +3,25 @@ package znet
 import (
 	"github.com/ebar-go/ego/utils/pool"
 	"github.com/ebar-go/znet/codec"
-	"sync"
 )
 
 // Engine provide context/handler management
 type Engine struct {
 	handleChains []HandleFunc // is a list of handlers
 
-	once            sync.Once
 	contextProvider pool.Provider[*Context] // is a pool for Context
 }
 
 func NewEngine() *Engine {
-	return &Engine{}
+	e := &Engine{}
+	e.contextProvider = pool.NewSyncPoolProvider[*Context](func() interface{} {
+		return &Context{engine: e}
+	})
+	return e
 }
 
 func (e *Engine) Use(handlers ...HandleFunc) {
 	e.handleChains = append(e.handleChains, handlers...)
-}
-
-func (e *Engine) getProvider() pool.Provider[*Context] {
-	e.once.Do(func() {
-		e.contextProvider = pool.NewSyncPoolProvider[*Context](func() interface{} {
-			return &Context{engine: e}
-		})
-	})
-	return e.contextProvider
 }
 
 // invoke process context handler chain
@@ -41,11 +34,10 @@ func (e *Engine) invoke(ctx *Context, index int8) {
 
 // compute run invoke function with context
 func (e *Engine) compute(conn *Connection, packet *codec.Packet) {
-	provider := e.getProvider()
 	// acquire context from provider
-	ctx := provider.Acquire()
+	ctx := e.contextProvider.Acquire()
 	ctx.reset(conn, packet)
-	defer provider.Release(ctx)
+	defer e.contextProvider.Release(ctx)
 
 	e.invoke(ctx, 0)
 }
